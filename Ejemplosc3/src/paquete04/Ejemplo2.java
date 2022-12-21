03a46919e1d9e0ac2c0be8d1cd654082968c3a3b
Richard Ortiz <raortiz20@utpl.edu.ec>
2022-12-21 15:54:32 -0500

    Ejercicios

--- Ejemplosc3/src/paquete04/Ejemplo2.java
+++ Ejemplosc3/src/paquete04/Ejemplo2.java
@@ -16,13 +16,39 @@ public class Ejemplo2 {
      */
     public static void main(String[] args) {
         // Creación de arreglo bidimensionales
-        
-        double[][] dato1 = {{1, 2, 3},{6, 8, 9}}; //
+
+        double operacion;
+        int f;
+        int c;
+
+        double[][] dato1 = {{1, 2, 3}, {6, 8, 9}};
+        double[][] dato2 = new double[2][3];
         /*
             1   4   3
             36  64  9
-        */
+         */
+
+        for (f = 0; f < dato1.length; f++) {
+            for (c = 0; c < dato1[f].length; c++) {
+
+                if (dato1[f][c] % 2 == 0) {
+                    dato2[f][c] = dato1[f][c] * dato1[f][c];
+                } else {
+                    dato2[f][c] = dato1[f][c];
+                }
+                }
+            }
+
+            for (f = 0; f < dato1.length; f++) {
+                for (c = 0; c < dato1[f].length; c++) {
+                    System.out.printf("%.0f\t", dato2[f][c]);
+                }
+                System.out.println();
+            }
+            
+          
         
+
     }
-    
+
 }
